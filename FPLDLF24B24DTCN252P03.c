adc74034823cc5a7c02702af11952f6826fd3e8e
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-12-14 14:22:08 +0700

    Add files via upload

---
+++ FPLDLF24B24DTCN252P03.c
@@ -0,0 +1,18 @@
+/*
+  Ho va ten: Nguyen Van Tuan Nam
+  Ma sinh vien: B24DTCN252
+  Bai 03
+*/
+/*
+  (1) int x;
+  (2) printf("%d",x);
+  (3) int y = 2;
+  (4) printf("%d",y+4);
+  -Su khac biet cua (1) va (3):
+  +(1) Chi khai bao x ma khong khoi tao gia tri.Gia tri cua x la khong xac dinh
+  +(3) Khai bao y dong thoi khoi tao gia tri ban dau la 2
+  -Ket qua cua (2):
+  Vi gia tri cua x la khong xac dinh nen chuong trinh in ra 0
+  -Ket qua cua (4):
+  Vi gia tri cua y la 2 nen chuong trinh in ra 6
+*/
