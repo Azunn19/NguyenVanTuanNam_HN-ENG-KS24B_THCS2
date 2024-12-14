5f5f637e9216244d2270a149fd073dd1a414e7c0
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-12-14 14:21:54 +0700

    Add files via upload

---
+++ FPLDLF24B24DTCN252P02.c
@@ -0,0 +1,14 @@
+/*
+  Ho va ten: Nguyen Van Tuan Nam
+  Ma sinh vien: B24DTCN252
+  Bai 02
+*/
+#include <stdio.h>
+
+int main() {
+    unsigned char B24DTCN252;
+    B24DTCN252 = 252;
+    printf("%u\n", B24DTCN252);
+
+    return 0;
+}
